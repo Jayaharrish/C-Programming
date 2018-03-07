6998561f8b4f2f93fd079351ba1a9457b18a8370
Jayaharrish <34646173+Jayaharrish@users.noreply.github.com>
2018-03-07 19:55:45 +0530

    Create set5.c

---
+++ set5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int num1,num2,num3;
+	printf("Enter three numbers:");
+	scanf("%d,%d,%d",&num1,&num2,&num3);
+	if(num1 > num2 && num1 > num3)
+		printf("\nNum1 is largest");
+	else if(num2 > num1 && num2 > num3)
+		printf("\nNum2 is largest");
+	else if(num3 > num1 && num3 > num2)
+		printf("\nNum3 is largest");
+	return 0;
+}
