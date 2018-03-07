c0f49455a8fd8b0ed90dadb0474f46dce6534ea2
Jayaharrish <34646173+Jayaharrish@users.noreply.github.com>
2018-03-07 20:05:21 +0530

    Create set8.c

---
+++ set8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int i,n,sum=0;
+printf("\n enter the value of n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+sum=sum+i;
+}
+printf("\n %d is sum of natural num",sum);
+}
